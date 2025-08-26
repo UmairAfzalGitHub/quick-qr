df411093295e146646263006bc24c93a2ba342c3
umair-bandlab <35869556+UmairAfzalGitHub@users.noreply.github.com>
2025-08-26 17:17:25 +0500

    WIP

--- Quick QR/Quick QR/Quick QR/Application/Modules/Launch/Splash/SplashViewController.swift
+++ Quick QR/Quick QR/Quick QR/Application/Modules/Launch/Splash/SplashViewController.swift
@@ -2,153 +2,87 @@
 import UIKit
 import Lottie
 import IOS_Helpers
+import GoogleMobileAds
 
-class SplashViewController: UIViewController, UITextViewDelegate {
+class SplashViewController: BaseViewController, UITextViewDelegate {
     
-    @IBOutlet weak var bottomStackView: UIStackView!
-    @IBOutlet weak var linksTextView: UITextView!
-    @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var actionButton: AppButton!
+    @IBOutlet weak var bannerViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var loadingAnimationView: LottieAnimationView!
-    
-    var reviewEnabled: Bool?
-    var splashAdEnabled: Bool?
-    var onBoardingStatus: Bool?
+    @IBOutlet weak var appTitleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        hideCustomNavigationBar()
         IAPManager.shared.fetchSubscriptions()
-        
-        // Check subscription status using the property
-        if IAPManager.shared.isUserSubscribed {
-            checkTermsAndConditions()
-            setupLinksTextView()
-        } else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: { [weak self] in
-                guard let self = self else { return }
+        IAPManager.shared.checkSubscriptionStatus(completion: {[weak self] isSubscribed in
+            self?.bannerViewHeightConstraint.constant = isSubscribed ? 0 : 60
+            guard let self, !isSubscribed else {
+                self?.animateForTwoSeconds()
+                return
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
                 AdsConsentManager.shared.checkAdsState {
                     AdManager.shared.setupAds()
-                    self.checkTermsAndConditions()
-                    self.setupLinksTextView()
+                    self.animateForTwoSeconds()
                 }
             })
-        }
+        })
     }
-    
-    func setup() {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.setupBanner(adId: AdMobConfig.banner)
+        super.viewWillAppear(animated)
+        localize()
+    }
+
+    override func setup() {
+        super.setup()
         loadingAnimationView.loopMode = .loop
-        loadingAnimationView.animationSpeed = 1
+        loadingAnimationView.animationSpeed = 0.65
         loadingAnimationView.play()
-        
-        actionButton.setTitle("Agree & Continue", for: .normal)
-        actionButton.setupButton(type: .primary)
-        actionButton.layer.cornerRadius = 8
     }
-    
-    func checkOnboardingStatus() {
-        UIApplication.shared.updateRootViewController(to: HomeViewContoller())
-        return  // remove this and above line in actual app
-        
-        // Uncomment this in actual app
-//        splashAdEnabled = RemoteConfigManager.shared.splashInterstitialEnabled
-//        reviewEnabled = RemoteConfigManager.shared.onboardingReviewEnabled
-//        onBoardingStatus = UserDefaultManager.shared.getValue(.onBoarding(false)) as? Bool ?? false
-//        
-//        if splashAdEnabled ?? true {
-//            AdManager.shared.loadInterstitialAd(id: AdMobConfig.interstitial) { isLoaded, interstitial in
-//                self.startSplash()
-//            }
-//        } else {
-//            self.startSplash()
-//        }
+
+    func localize() {
+        appTitleLabel.text = Strings.Label.phoneNumberLocator
     }
-    
-    func startSplash() {
-        
-        // If onboarding is not complete and review is enabled, show onboarding first
-        if !(self.onBoardingStatus ?? true) && self.reviewEnabled ?? false {
-            UIApplication.shared.updateRootViewController(to: OnboardingViewController())
-            return
-        }
-        
-        let nextController = HomeViewContoller()
-        if RemoteConfigManager.shared.splashInterstitialEnabled {
-            AdManager.shared.adCounter = RemoteConfigManager.shared.maxInterstitalAdCounter
-        }
-        AdManager.shared.showInterstitial(adId: AdMobConfig.interstitial) {
-            UIApplication.shared.updateRootViewController(to: nextController)
-        }
-        return
-    }
-    
-    func checkTermsAndConditions() {
-        let termsStatus = UserDefaultManager.shared.getValue(.termsAccepted(false)) as? Bool ?? false
-        if termsStatus {
-            loadingAnimationView.isHidden = false
-            bottomStackView.isHidden = true
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { //Custom Wait
-                self.checkOnboardingStatus()
-            })
+
+    func checkLanguageStatus() {
+        let onBoardingStatus = UserDefaults.standard.bool(forKey: "isOnboardingComplete")
+        if onBoardingStatus {
+            AdManager.shared.loadInterstitialAd(id: AdMobConfig.interstitial) { isLoaded, interstitial in
+//                let nextController = UINavigationController(rootViewController: HomeViewController())
+//                nextController.isNavigationBarHidden = true
+//                if AdManager.shared.splashInterstitial {
+//                    AdManager.shared.adCounter = AdManager.shared.maxInterstitalAdCounter
+//                }
+//                AdManager.shared.showInterstitial(adId: AdMobConfig.interstitial) {
+//                    UIApplication.shared.updateRootViewController(to: nextController)
+//                }
+            }
         } else {
-            loadingAnimationView.isHidden = true
-            bottomStackView.isHidden = false
+            AdManager.shared.loadNativeAd(adId: AdMobConfig.native,
+                                          from: self) { nativeAd in
+//                let controller = LanguageViewController(viewMode: .main,
+//                                                        intent: .onboarding,
+//                                                        nativeAd: nativeAd)
+//                let navController = UINavigationController(rootViewController: controller)
+//                UIApplication.shared.updateRootViewController(to: navController)
+            }
         }
+    }
+    
+    func animateForTwoSeconds() {
+        loadingAnimationView.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0, execute: {
+            self.checkLanguageStatus()
+        })
     }
     
     @IBAction func didTapActionButton(_ sender: Any) {
-        UserDefaultManager.shared.setValue(.termsAccepted(true))
-        self.checkOnboardingStatus()
+        UserDefaults.standard.set(true, forKey: "isTermAccecpted")
+        UserDefaults.standard.synchronize()
+        self.checkLanguageStatus()
         self.loadingAnimationView.stop() //This is important
-    }
-    
-    private func setupLinksTextView() {
-        linksTextView.backgroundColor = .clear
-        linksTextView.textAlignment = .center
-        linksTextView.isEditable = false
-        linksTextView.isScrollEnabled = false
-        linksTextView.textAlignment = .center
-        linksTextView.font = UIFont.systemFont(ofSize: 13.0) // Default font for UITextView
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-        
-        let fullText = "Privacy For further information, please check our Privacy Policy and Terms & Conditions."
-        let attributedString = NSMutableAttributedString(string: fullText, attributes: [
-            .foregroundColor: UIColor.textPrimary,
-            .font: UIFont.systemFont(ofSize: 13.0),
-            .paragraphStyle: paragraphStyle // Apply font to the whole text
-        ])
-        
-        // Add links
-        let privacyPolicyRange = (fullText as NSString).range(of: "Privacy Policy")
-        let termsAndConditionsRange = (fullText as NSString).range(of: "Terms & Conditions")
-        
-        attributedString.addAttributes([
-            .link: URL(string: "https://doc-hosting.flycricket.io/photo-recovery-videos-recovery-privacy-policy/b1727af7-f37a-4686-b37d-f925a1e26218/privacy")!,
-            .font: UIFont.systemFont(ofSize: 13.0), // Apply link-specific font
-            .foregroundColor: UIColor.appGreenMedium
-        ], range: privacyPolicyRange)
-        
-        attributedString.addAttributes([
-            .link: URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/")!,
-            .font: UIFont.systemFont(ofSize: 13.0), // Apply link-specific font
-            .foregroundColor: UIColor.appGreenMedium
-        ], range: termsAndConditionsRange)
-        
-        linksTextView.attributedText = attributedString
-        linksTextView.delegate = self
-    }
-    
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-        openURL(URL.absoluteString)
-        return false // Prevent default behavior
-    }
-    
-    private func openURL(_ urlString: String) {
-        if let url = URL(string: urlString) {
-            UIApplication.shared.open(url)
-        }
     }
 }
