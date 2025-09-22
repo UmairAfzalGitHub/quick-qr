4b9980ee5364cbbf1324d4c72a342993d060083c
umair-bandlab <35869556+UmairAfzalGitHub@users.noreply.github.com>
2025-09-22 21:00:32 +0500

    fix for native ad view

--- Quick QR/Quick QR/Quick QR/Application/Modules/App/CodeGenerator/CodeGeneratorViewController.swift
+++ Quick QR/Quick QR/Quick QR/Application/Modules/App/CodeGenerator/CodeGeneratorViewController.swift
@@ -779,12 +779,18 @@ class CodeGeneratorViewController: UIViewController {
     
     private func showGoogleNativeAd(nativeAd: GoogleMobileAds.NativeAd?) {
         guard let nativeAd else { return }
-        let nibView = Bundle.main.loadNibNamed("OnBoardingNativeAdView", owner: nil, options: nil)?.first
+        let nibName = UIDevice().isSmallerDevice() ? "NativeAdView" : "OnBoardingNativeAdView"
+        let nibView = Bundle.main.loadNibNamed(nibName, owner: nil, options: nil)?.first
         guard let nativeAdView = nibView as? NativeAdView else { return }
         setAdView(nativeAdView)
 
-        (nativeAdView.headlineView as? UILabel)?.text = nativeAd.headline
-        nativeAdView.mediaView?.mediaContent = nativeAd.mediaContent
+        if UIDevice().isSmallerDevice() {
+            nativeAdView.mediaView?.isHidden = true
+            nativeAdView.mediaView?.removeFromSuperview()
+        } else {
+            (nativeAdView.headlineView as? UILabel)?.text = nativeAd.headline
+            nativeAdView.mediaView?.mediaContent = nativeAd.mediaContent
+        }
 
         // Configure optional assets
         (nativeAdView.bodyView as? UILabel)?.text = nativeAd.body
@@ -795,17 +801,16 @@ class CodeGeneratorViewController: UIViewController {
         nativeAdView.callToActionView?.layer.cornerRadius = 12.0
         
         (nativeAdView.iconView as? UIImageView)?.image = nativeAd.icon?.image
-        nativeAdView.iconView?.isHidden = nativeAd.icon == nil
+//        nativeAdView.iconView?.isHidden = nativeAd.icon == nil
         
         (nativeAdView.advertiserView as? UILabel)?.text = nativeAd.advertiser
-        nativeAdView.advertiserView?.isHidden = nativeAd.advertiser == nil
+//        nativeAdView.advertiserView?.isHidden = nativeAd.advertiser == nil
         
         // Disable user interaction on call-to-action view for SDK to handle touches
         nativeAdView.callToActionView?.isUserInteractionEnabled = false
         
         nativeAdView.nativeAd = nativeAd
     }
-    
     
     // MARK: - Public Methods
     
