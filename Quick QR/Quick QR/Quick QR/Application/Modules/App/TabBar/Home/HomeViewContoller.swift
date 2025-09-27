eb6fef088bc2e159bc97650af78023b79d8173ef
Haider Rathore <70682547+Haiderrathore@users.noreply.github.com>
2025-09-27 17:03:41 +0500

    Collection reloaded

--- Quick QR/Quick QR/Quick QR/Application/Modules/App/TabBar/Home/HomeViewContoller.swift
+++ Quick QR/Quick QR/Quick QR/Application/Modules/App/TabBar/Home/HomeViewContoller.swift
@@ -57,7 +57,7 @@ class HomeViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        collectionView.reloadData()
         loadNativeAdIfNeeded()
     }
     
