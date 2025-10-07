6ba5ad184f3fc05789e856a22f8ea605669661ca
Haider Rathore <70682547+Haiderrathore@users.noreply.github.com>
2025-10-07 20:28:59 +0500

    Tabbar custom show hide

--- Quick QR/Quick QR/Quick QR/Application/Modules/App/CodeGenerator/CodeGenerationResultViewController.swift
+++ Quick QR/Quick QR/Quick QR/Application/Modules/App/CodeGenerator/CodeGenerationResultViewController.swift
@@ -85,8 +85,17 @@ class CodeGenerationResultViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        if let tabBarController = self.tabBarController as? TabBarController {
+            tabBarController.setTabBarVisibility(true)  // Hide tab bar
+        }
         loadNativeAdIfNeeded()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let tabBarController = self.tabBarController as? TabBarController {
+            tabBarController.setTabBarVisibility(false)  // Show tab bar
+        }
     }
 
     private func loadNativeAdIfNeeded() {
